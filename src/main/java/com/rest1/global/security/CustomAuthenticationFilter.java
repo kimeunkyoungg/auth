8943c52ee570688b180bdc22f7e773767a3f545f
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-09-22 15:46:28 +0900

    CustomAuthenticationFilter 수정

--- src/main/java/com/rest1/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/rest1/global/security/CustomAuthenticationFilter.java
@@ -1,21 +1,99 @@
 package com.rest1.global.security;
 
+import com.rest1.domain.member.member.entity.Member;
+import com.rest1.domain.member.member.service.MemberService;
+import com.rest1.global.exception.ServiceException;
+import com.rest1.global.rq.Rq;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 //시큐리티 필터 이전에 사용하는 것. 회사 직원의 역할
+//JWT
+
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationFilter extends OncePerRequestFilter {
+
+    private final MemberService memberService;
+    private final Rq rq;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         logger.debug("CustomAuthenticationFilter called");
-        System.out.println("hihi");
-        filterChain.doFilter(request, response); //이걸 해줘야지만 위의 과정 후 이후로 지나가게 된다.
+
+        //JWT 인증 방식아니면 넘기기
+        if(!request.getRequestURI().startsWith("/api/")) {
+            filterChain.doFilter(request, response); //넘겨버리기
+            return;
+        }
+
+        //회원가입과 로그인은 인증을 할 필요가 없다
+        if(List.of("/api/v1/members/join", "/api/v1/members/login").contains(request.getRequestURI())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String apiKey;
+        String accessToken;
+
+        String headerAuthorization = rq.getHeader("Authorization", "");
+
+        if (!headerAuthorization.isBlank()) {
+            if (!headerAuthorization.startsWith("Bearer "))
+                throw new ServiceException("401-2", "Authorization 헤더가 Bearer 형식이 아닙니다.");
+
+            String[] headerAuthorizationBits = headerAuthorization.split(" ", 3);
+
+            apiKey = headerAuthorizationBits[1];
+            accessToken = headerAuthorizationBits.length == 3 ? headerAuthorizationBits[2] : "";
+        } else {
+            apiKey = rq.getCookieValue("apiKey", "");
+            accessToken = rq.getCookieValue("accessToken", "");
+        }
+
+        if (apiKey.isBlank())
+            throw new ServiceException("401-1", "로그인 후 이용해주세요.");
+
+        Member member = null;
+
+        boolean isAccessTokenExists = !accessToken.isBlank();
+        boolean isAccessTokenValid = false;
+
+        if (isAccessTokenExists) {
+            Map<String, Object> payload = memberService.payloadOrNull(accessToken);
+
+            if (payload != null) {
+                long id = (long) payload.get("id");
+                String username = (String) payload.get("username");
+                String nickname = (String) payload.get("nickname");
+
+                member = new Member(id, username, nickname);
+                isAccessTokenValid = true;
+            }
+        }
+
+        if (member == null) {
+            member = memberService
+                    .findByApiKey(apiKey)
+                    .orElseThrow(() -> new ServiceException("401-3", "API 키가 유효하지 않습니다."));
+        }
+
+        if (isAccessTokenExists && !isAccessTokenValid) {
+            String newAccessToken = memberService.genAccessToken(member);
+            rq.setCookie("accessToken", newAccessToken);
+            rq.setHeader("accessToken", newAccessToken);
+        }
+
+
+
     }
 }
